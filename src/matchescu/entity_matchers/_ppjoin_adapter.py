366c0dcde8e4f6d3e1b820311921d18215e70b47
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-10-21 21:44:45 +0300

    make changes to how the algebraic and serf metrics are computed

--- src/matchescu/entity_matchers/_ppjoin_adapter.py
+++ src/matchescu/entity_matchers/_ppjoin_adapter.py
@@ -18,35 +18,29 @@ def _compute_fsm(input_data: list[DataFrame], result: Iterable[tuple[tuple]]) ->
         yield item1, item2
 
 
-def _get_partitioned_set(input_data: Iterable[DataFrame]) -> Generator[tuple, None, None]:
-    yield from (
-        (df_idx, idx)
-        for df_idx, df in enumerate(input_data)
-        for idx in df.index
-    )
+def _compute_partition(input_data: list[DataFrame], result: Iterable[tuple[tuple]]) -> frozenset:
+    partition = dict()
+    for r in result:
+        ds1_id, r1id = r[0]
+        ds2_id, r2id = r[1]
+        item1 = tuple(v for v in input_data[ds1_id].iloc[r1id, :])
+        item2 = tuple(v for v in input_data[ds2_id].iloc[r2id, :])
+        cluster = (item1, item2)
+        partition[cluster] = partition.get(cluster, 0) + 1
+    return frozenset(partition)
 
 
-def _map_to_data(input_data: list[DataFrame]) -> Callable[[Iterable[tuple]], Iterable[tuple]]:
-    def _map(equivalence_class: Iterable[tuple]) -> Iterable[tuple]:
-        ret = []
-        for df_id, row_id in equivalence_class:
-            row = tuple(value for value in input_data[df_id].iloc[row_id, :])
-            ret.append(row)
-        return ret
-    return _map
-
-
-def _compute_algebraic(input_data: list[DataFrame], partition: set) -> Generator[tuple, None, None]:
-    yield from map(_map_to_data(input_data), partition)
+def _compute_algebraic(partition: frozenset[tuple[tuple]]) -> Generator[tuple, None, None]:
+    for item in partition:
+        yield item
 
 
 def _compute_serf(
-    input_data: list[DataFrame],
-    partition: set,
+    partition: frozenset[tuple[tuple]],
     merge_function: Callable[[Record, Record], Record],
-):
-    for eq_class in map(_map_to_data(input_data), partition):
-        merged_eq_class = reduce(merge_function, eq_class)
+) -> Generator[tuple, None, None]:
+    for cluster in partition:
+        merged_eq_class = reduce(merge_function, cluster)
         yield merged_eq_class
 
 
@@ -66,9 +60,9 @@ def ppjoin_adapter(
 
     er_result = EntityResolutionResult()
     er_result.fsm = list(_compute_fsm(input_data, result))
-    # partition = compute_partition(list(_get_partitioned_set(input_data)), result)
-    # er_result.algebraic = list(_compute_algebraic(input_data, partition))
-    # if merge_function is not None:
-    #     er_result.serf = list(_compute_serf(input_data, partition, merge_function))
+    partition = _compute_partition(input_data, result)
+    er_result.algebraic = list(_compute_algebraic(partition))
+    if merge_function is not None:
+        er_result.serf = list(_compute_serf(partition, merge_function))
 
     return er_result
