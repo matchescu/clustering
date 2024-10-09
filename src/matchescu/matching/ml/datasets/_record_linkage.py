742676f5d93a8e13a599ed8155b1ae816df6d4d9
Andrei Olar <andrei.olar@gmail.com>
2024-10-09 14:25:36 +0300

    extract entity reference comparison logic

--- src/matchescu/matching/ml/datasets/_record_linkage.py
+++ src/matchescu/matching/ml/datasets/_record_linkage.py
@@ -1,5 +1,4 @@
-from functools import partial
-from typing import Any
+from typing import Any, Callable, Hashable
 
 import polars as pl
 
@@ -9,6 +8,46 @@ from matchescu.matching.entity_reference import (
     EntityReferenceComparisonConfig,
 )
 from matchescu.typing import DataSource, Record, EntityReference
+
+
+class AttributeComparison:
+    def __init__(
+        self,
+        ground_truth: set[tuple[Hashable, Hashable]],
+        cmp_config: EntityReferenceComparisonConfig,
+        cmp_row_separator: int,
+        left_id: Callable[[EntityReference], Hashable],
+        right_id: Callable[[EntityReference], Hashable],
+        target_col_name: str,
+    ):
+        self._gt = ground_truth
+        self._config = cmp_config
+        self._row_sep = cmp_row_separator
+        self._left_id = left_id
+        self._right_id = right_id
+        self._target_col = target_col_name
+
+    @staticmethod
+    def __compare_attr_values(
+        left_ref: EntityReference,
+        right_ref: EntityReference,
+        config: AttrComparisonSpec,
+    ) -> int:
+        a = left_ref[config.left_ref_key]
+        b = right_ref[config.right_ref_key]
+        return config.match_strategy(a, b)
+
+    def __call__(self, sample: tuple) -> tuple[dict]:
+        left_side = sample[: self._row_sep]
+        right_side = sample[self._row_sep :]
+        result = {
+            spec.label: self.__compare_attr_values(left_side, right_side, spec)
+            for spec in self._config.specs
+        }
+        result[self._target_col] = int(
+            (self._left_id(left_side), self._right_id(right_side)) in self._gt
+        )
+        return (result,)  # need to return a tuple
 
 
 class RecordLinkageDataSet:
@@ -44,44 +83,23 @@ class RecordLinkageDataSet:
         df = pl.DataFrame(extract())
         return df.rename({key: f"{key}{suffix}" for key in df.columns})
 
-    @staticmethod
-    def __compare_attr_values(
-        left_ref: EntityReference,
-        right_ref: EntityReference,
-        config: AttrComparisonSpec,
-    ) -> int:
-        a = left_ref[config.left_ref_key]
-        b = right_ref[config.right_ref_key]
-        return config.match_strategy(a, b)
-
-    def __compare_entity_references(
-        self,
-        comparison_row: tuple,
-        midpoint: int,
-        config: EntityReferenceComparisonConfig,
-    ) -> tuple:
-        left_side = comparison_row[:midpoint]
-        right_side = comparison_row[midpoint:]
-        left_id = self.__extract_left.identify(left_side)
-        right_id = self.__extract_right.identify(right_side)
-        result = {
-            spec.label: self.__compare_attr_values(left_side, right_side, spec)
-            for spec in config.specs
-        }
-        result[self.__TARGET_COL] = int((left_id, right_id) in self.__true_matches)
-        return (result,)  # need to return a tuple
-
     def create_comparison_matrix(
-        self, config: EntityReferenceComparisonConfig
+        self,
+        config: EntityReferenceComparisonConfig,
+        reference_comparer: Callable[[tuple], tuple[dict]] | None = None,
     ) -> "RecordLinkageDataSet":
         left = self.__with_col_suffix(self.__extract_left, "_left")
         right = self.__with_col_suffix(self.__extract_right, "_right")
         cross_product = left.join(right, how="cross")
-        compare_entity_references = partial(
-            self.__compare_entity_references, midpoint=len(left.columns), config=config
+        reference_comparer = reference_comparer or AttributeComparison(
+            self.__true_matches,
+            config,
+            len(left.columns),
+            self.__extract_left.identify,
+            self.__extract_right.identify,
+            self.__TARGET_COL,
         )
-        dtype = {spec.label: pl.UInt8 for spec in config.specs}
-        self.__comparison_data = cross_product.map_rows(
-            compare_entity_references, pl.Struct(dtype)
-        ).unnest("column_0")
+        self.__comparison_data = cross_product.map_rows(reference_comparer).unnest(
+            "column_0"
+        )
         return self
