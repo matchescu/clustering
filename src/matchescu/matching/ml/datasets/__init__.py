4739b99cc24e638ef9a865473ef19a31d16207fd
Andrei Olar <andrei.olar@gmail.com>
2024-10-14 10:30:30 +0300

    Use patterned weights to encode feature values (#12)
    * extract entity reference comparison logic
    
    * extract superclass for sample factories
    
    * allow encoding desired patterns into the comparison results
    
    * use patterns to encode features for logistic regression
    
    * rename create_comparison_matrix -> cross_sources

--- src/matchescu/matching/ml/datasets/__init__.py
+++ src/matchescu/matching/ml/datasets/__init__.py
@@ -1,6 +1,16 @@
 from matchescu.matching.ml.datasets._csv import CsvDataSource
-from matchescu.matching.ml.datasets._record_linkage import RecordLinkageDataSet
+from matchescu.matching.ml.datasets._record_linkage import (
+    RecordLinkageDataSet,
+    AttributeComparison,
+    PatternEncodedComparison,
+)
 from matchescu.matching.ml.datasets._traits import Traits
 
 
-__all__ = ["CsvDataSource", "RecordLinkageDataSet", "Traits"]
+__all__ = [
+    "CsvDataSource",
+    "RecordLinkageDataSet",
+    "Traits",
+    "AttributeComparison",
+    "PatternEncodedComparison",
+]
