49a11e0d2fa9905fabcfea425e19673114669351
Andrei Olar <andrei.olar@gmail.com>
2025-03-31 14:32:09 +0300

    Implement correlation clustering using a random pivot (#16)
    * In weighted directed graphs, correlation clustering has to account for a lack of edge symmetry. This implementation picks nodes at random from previously unclustered nodes resulting in different outcomes. Controlling the random generator is possible by passing a seed value.
    * Bump version to 0.3.0
    * Add clustering dependency on reference store

--- src/matchescu/clustering/__init__.py
+++ src/matchescu/clustering/__init__.py
@@ -1,5 +1,10 @@
-from matchescu.clustering._cc import ConnectedComponents
+from matchescu.clustering._corr import WeightedCorrelationClustering
+from matchescu.clustering._wcc import WeaklyConnectedComponents
 from matchescu.clustering._ecp import EquivalenceClassPartitioner
 
 
-__all__ = ["ConnectedComponents", "EquivalenceClassPartitioner"]
+__all__ = [
+    "EquivalenceClassPartitioner",
+    "WeaklyConnectedComponents",
+    "WeightedCorrelationClustering",
+]
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
+import itertools
+from functools import reduce
 from pathlib import Path
 from typing import Hashable, Callable
+from unittest.mock import MagicMock
 
 import pytest
 
-from matchescu.typing import EntityReferenceIdentifier
+from matchescu.reference_store.comparison_space import BinaryComparisonSpace
+from matchescu.similarity import SimilarityGraph, Matcher
+from matchescu.typing import EntityReferenceIdentifier, EntityReference
 
 
 @pytest.fixture(scope="session")
@@ -16,16 +21,77 @@ def data_dir(test_dir):
     return test_dir / "data"
 
 
+@pytest.fixture
+def source():
+    return "test"
+
+
 @pytest.fixture(scope="session")
 def ref_id() -> Callable[[Hashable, str], EntityReferenceIdentifier]:
     return lambda lbl, src: EntityReferenceIdentifier(lbl, src)
 
 
+@pytest.fixture(scope="session")
+def ref(ref_id):
+    def _(lbl, src):
+        mock = MagicMock(name=f"{lbl}-reference", spec=EntityReference)
+        mock.id = ref_id(lbl, src)
+        return mock
+
+    return _
+
+
 @pytest.fixture
-def entity_reference_id_set(ref_id) -> list[EntityReferenceIdentifier]:
+def entity_reference_id_set(ref_id, source) -> list[EntityReferenceIdentifier]:
     return [
-        ref_id("a", "test"),
-        ref_id("b", "test"),
-        ref_id("c", "test"),
-        ref_id("d", "test"),
+        ref_id("a", source),
+        ref_id("b", source),
+        ref_id("c", source),
+        ref_id("d", source),
     ]
+
+
+@pytest.fixture
+def comparison_space(entity_reference_id_set, request, ref_id, source):
+    result = MagicMock(spec=BinaryComparisonSpace)
+    cmp_space = list(itertools.combinations(entity_reference_id_set, 2))
+    if hasattr(request, "param") and isinstance(request.param, list):
+        cmp_space = [(ref_id(x, source), ref_id(y, source)) for x, y in request.param]
+    result.__iter__.return_value = cmp_space
+    return result
+
+
+@pytest.fixture
+def max_non_match_threshold(request):
+    return request.param if hasattr(request, "param") else 0.25
+
+
+@pytest.fixture
+def min_match_threshold(request):
+    return request.param if hasattr(request, "param") else 0.75
+
+
+@pytest.fixture
+def matcher_mock(request):
+    result = MagicMock(name="mock matcher object", spec=Matcher)
+    result.return_value = request.param if hasattr(request, "param") else 1.0
+    return result
+
+
+@pytest.fixture
+def similarity_graph(
+    ref, matcher_mock, max_non_match_threshold, min_match_threshold, source, request
+):
+    edge_spec = [
+        (ref("a", source), ref("b", source)),
+        (ref("b", source), ref("c", source)),
+        (ref("c", source), ref("d", source)),
+    ]
+    if hasattr(request, "param") and isinstance(request.param, (list, set, tuple)):
+        edge_spec = list((ref(x, source), ref(y, source)) for x, y in request.param)
+    sim_graph = reduce(
+        lambda g, pair: g.add(*pair),
+        edge_spec,
+        SimilarityGraph(matcher_mock, max_non_match_threshold, min_match_threshold),
+    )
+    return sim_graph
